1dca70be920909e149d98ac441fc6522043a9b2d
frankregel <frank.regel@gmail.com>
2014-01-31 18:46:46 +0100

    Initial Commit

---
+++ Datensauger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Datensauger
+//
+//  Created by Frank Regel on 31.01.14.
+//  Copyright (c) 2014 Frank Regel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DatensaugerTests/DatensaugerTests.m
@@ -0,0 +1,34 @@
+//
+//  DatensaugerTests.m
+//  DatensaugerTests
+//
+//  Created by Frank Regel on 31.01.14.
+//  Copyright (c) 2014 Frank Regel. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DatensaugerTests : XCTestCase
+
+@end
+
+@implementation DatensaugerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
